f13b487cfb173cc9b2441ce7221580b6eec1236b
Keith Smith <keith.smith@tvdsb.ca>
2020-08-29 19:49:47 +0000

    Added an underline to song title and fixed compiling issue

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Song title");
-    /* Delete this comment and add your lyrics here. 
-    Remember to keep the symbols and lines the same, but
-    each line must be in its own System.out.println ("");
-    Otherwise the program may not work right */
+    System.out.println("Song title");// add a song title
+    System.out.println("---- -----");
+    /*
+     * Delete this comment and add your lyrics here. Remember to keep the symbols
+     * and lines the same, but each line must be in its own System.out.println ("");
+     * Otherwise the program may not work right
+     */
   }
 }
