6f476b599aa667879af3f43ee04427975e3eb8a3
Keith Smith <keith.smith@tvdsb.ca>
2020-08-29 19:41:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Song title");
+    /* Delete this comment and add your lyrics here. 
+    Remember to keep the symbols and lines the same, but
+    each line must be in its own System.out.println ("");
+    Otherwise the program may not work right */
+  }
+}
